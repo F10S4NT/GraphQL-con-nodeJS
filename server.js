41ed30427d00659f0e1d4cde937e3d911bb82933
F10S4NT <ugflosant@gmail.com>
2022-11-16 15:40:54 -0600

    se agrego schema para consultas

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const {buildSchema} = require('graphql')
 
 const app = express();
+
+// esquema de graphql
+const schema = buildSchema(`
+  type Course {
+    id: ID
+    title: String!
+    views: Int
+  }
+
+  type Query{
+    getCourses: [Course] 
+  }
+`);//template string
 
 app.get('/', function(req, res){
     res.send(("Bienvenido"));
